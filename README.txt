fdbd788cf52f83d5b8747bf9fef93352ddd043a2
Franco Ruggeri <franco.ruggeri@studenti.polito.it>
2019-04-12 19:23:06 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,25 @@
+# README
+
+## Requirements
+* Add-on "cookies.txt" of Firefox
+* Curl
+
+#### How to install cookies.txt
+Download it at [cookies.txt](https://addons.mozilla.org/en-US/firefox/addon/cookies-txt/?src=search)
+
+#### How to install Curl
+Use the following command
+```bash
+sudo apt install curl
+```
+
+## How to use
+* Open firefox
+* Log in at www.polito.it and go to the webpage of the videolesson 1
+* Export cookies by clicking on the add-on icon (upper right corner)
+* Copy the webpage url of the videolesson 1
+* Run the script:
+	+ 1st parameter: cookies
+	+ 2nd parameter: url
+	+ 3rd parameter: path where to download videolessons
+	+ Example: ./download_videolessons.sh cookies.txt https://didattica.polito.it/portal/pls/portal/sviluppo.videolezioni.vis?cor=410&arg=Lezioni%20on-line&lez=17584 ./Videolessons
---
+++ download_videolessons.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+	echo "usage: $0 cookie url path_download"
+	exit 1
+fi
+
+# declarations
+base_url_video="https://didattica.polito.it"
+base_url_page=$base_url_video"/portal/pls/portal/"
+info="Info.html"
+cookie="$1"
+url_page=$2
+path_download="$3"
+tmp=$$
+
+# prepare environment
+mkdir $tmp
+mkdir -p "$path_download"
+
+# webpage
+curl -s --cookie "$cookie" $url_page > "$path_download"/$info
+course=$(cat "$path_download"/$info | grep 'h2 text-primary' | cut -d '>' -f 2 | \
+	cut -d '<' -f 1 | sed 's/^ *//g' | sed 's/ *$//g' | tr ' ' '_')  # leading and trailing white spaces removed
+echo "Course:" $course
+
+# download
+cat "$path_download"/$info | grep '<a style="color:#003576;' > $tmp/list
+while read line; do
+	# build url of webpage
+	url_page=$(echo $line | cut -d '"' -f 4)
+	echo $url_page > $tmp/url
+	url_page=$(awk '{gsub(/&amp;/, "\&", $0); print $0}' $tmp/url)	# replace &amp; with &
+	url_page=$base_url_page$url_page
+
+	# download webpage
+	curl -s --cookie "$cookie" $url_page > $tmp/$info
+
+	# build url and name of video
+	url_video=$base_url_video$(cat $tmp/$info | grep -e '<a id="video.*Video</a>' | cut -d '"' -f 4)
+	n=$(echo $line | cut -d '>' -f 2 | cut -d '<' -f 1 | cut -d ' ' -f 2)	# number of lesson
+	if [ $n = "Lezione" ]; then	# in old pages there is a space between <year> and "Lezione"
+		n=$(echo $line | cut -d '>' -f 2 | cut -d '<' -f 1 | cut -d ' ' -f 3)
+	fi
+	filename=$course"_lez_"$n".mp4"
+
+	# download video
+	found=0
+	for f in $(ls "$path_download"); do
+		if [ $f = $filename ]; then
+			found=1
+			break
+		fi
+	done
+	if [ $found -ne 1 ]; then
+		wget -O "$path_download"/$filename -q $url_video
+		echo "$filename downloaded"
+	else
+		echo "$filename already present"
+	fi
+done < $tmp/list
+
+# clean
+rm -rf $tmp
+
+exit 0
